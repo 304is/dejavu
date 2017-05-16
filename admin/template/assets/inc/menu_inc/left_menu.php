e0cc083071aafc0c858d04bbefb110fe48ffd6b0
redgold98 <anton.grobovenko@gmail.com>
2017-05-16 14:22:12 +0500

    Перевод в PHP

---
+++ admin/template/assets/inc/menu_inc/left_menu.php
@@ -0,0 +1,9 @@
+<nav id="menu">
+		<ul>
+				<li><a href="#"><i class="icon  fa fa-home"></i> Главная </a></li>
+				<li><a href="#"><i class="icon  fa fa-th-large"></i> Товары </a></li>
+				<li><a href="#"><i class="icon  fa fa-user"></i> Пользователи </a></li>
+				<li><a href="#"><i class="icon  fa fa-comment"></i> Отзывы </a></li>
+				<li><a href="#"><i class="icon  fa fa-shopping-cart"></i> Заказы </a></li>
+		</ul>
+</nav>
