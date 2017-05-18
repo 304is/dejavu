935b74ec33868aef373878b6b3153b40133b0055
Denis Popov <den_vas.88@mail.ru>
2017-05-18 15:24:49 +0500

    Добавлено подключение к базе данных и функции fetchAll и fetchOne

---
+++ lib/db.php
@@ -0,0 +1,42 @@
+<?php
+include_once "config.php";
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if (!$conn) {
+    echo "No connection database!";
+    exit();
+    }
+function fetchAll($sql){
+    global $conn;
+    $arr = [];
+    $res = mysqli_query($conn, $sql);
+    if (!$res) {
+        echo 'Îøèáêà: '.mysqli_errno($conn).': '.mysqli_error($conn);
+        exit();
+    }
+    if (mysqli_num_rows($res) == 0) {
+    
+        return false;
+    } else {
+        while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+            $arr[] = $row;
+        }
+        
+        return $arr;
+    }
+}
+function fetchOne($sql){
+    global $conn;
+    $res = mysqli_query($conn, $sql);
+    if (!$res) {
+        echo 'Îøèáêà: '.mysqli_errno($conn).': '.mysqli_error($conn);
+        exit();
+    }
+    if (mysqli_num_rows($res) == 0) {
+    
+        return false;
+    } else {
+        $row = mysqli_fetch_array($res, MYSQLI_ASSOC))
+        
+        return $row;
+    }
+}
