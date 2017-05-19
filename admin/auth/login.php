f3bfd72a660ea4a5f8bd4e50ad33a98e507c7183
redgold98 <anton.grobovenko@gmail.com>
2017-05-19 12:05:09 +0500

    Смена директории

--- admin/auth/login.php
+++ admin/auth/login.php
@@ -7,21 +7,21 @@
 <!-- Title-->
 <title>CAPLET |  Admin HTML Themes</title>
 <!-- Favicons -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/ico/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../template/assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../template/assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../template/assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="../template/assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="../template/assets/ico/favicon.ico">
 <!-- CSS Stylesheet-->
-<link type="text/css" rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css" />
-<link type="text/css" rel="stylesheet" href="assets/css/bootstrap/bootstrap-themes.css" />
-<link type="text/css" rel="stylesheet" href="assets/css/style.css" />
+<link type="text/css" rel="stylesheet" href="../template/assets/css/bootstrap/bootstrap.min.css" />
+<link type="text/css" rel="stylesheet" href="../template/assets/css/bootstrap/bootstrap-themes.css" />
+<link type="text/css" rel="stylesheet" href="../template/assets/css/style.css" />
 
 <!-- Styleswitch if  you don't chang theme , you can delete -->
-<link type="text/css" rel="alternate stylesheet" media="screen" title="style1" href="assets/css/styleTheme1.css" />
-<link type="text/css" rel="alternate stylesheet" media="screen" title="style2" href="assets/css/styleTheme2.css" />
-<link type="text/css" rel="alternate stylesheet" media="screen" title="style3" href="assets/css/styleTheme3.css" />
-<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="assets/css/styleTheme4.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style1" href="../template/assets/css/styleTheme1.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style2" href="../template/assets/css/styleTheme2.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style3" href="../template/assets/css/styleTheme3.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="../template/assets/css/styleTheme4.css" />
 
 </head>
 <body class="full-lg">
@@ -40,7 +40,6 @@
 								<div class="account-wall">
 										<section class="align-lg-center">
 										<div class="site-logo"></div>
-										<h1 class="login-title"><span>wel</span>come <small> CAPLET   Admin HTML Themes Version 1.1.3</small></h1>
 										</section>
 										<form id="form-signin" class="form-signin">
 												<section>
@@ -90,25 +89,25 @@
 -->
 		
 <!-- Jquery Library -->
-<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.ui.min.js"></script>
-<script type="text/javascript" src="assets/plugins/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="../template/assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="../template/assets/js/jquery.ui.min.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/bootstrap/bootstrap.min.js"></script>
 <!-- Modernizr Library For HTML5 And CSS3 -->
-<script type="text/javascript" src="assets/js/modernizr/modernizr.js"></script>
-<script type="text/javascript" src="assets/plugins/mmenu/jquery.mmenu.js"></script>
-<script type="text/javascript" src="assets/js/styleswitch.js"></script>
+<script type="text/javascript" src="../template/assets/js/modernizr/modernizr.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/mmenu/jquery.mmenu.js"></script>
+<script type="text/javascript" src="../template/assets/js/styleswitch.js"></script>
 <!-- Library 10+ Form plugins-->
-<script type="text/javascript" src="assets/plugins/form/form.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/form/form.js"></script>
 <!-- Datetime plugins -->
-<script type="text/javascript" src="assets/plugins/datetime/datetime.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/datetime/datetime.js"></script>
 <!-- Library Chart-->
-<script type="text/javascript" src="assets/plugins/chart/chart.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/chart/chart.js"></script>
 <!-- Library  5+ plugins for bootstrap -->
-<script type="text/javascript" src="assets/plugins/pluginsForBS/pluginsForBS.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/pluginsForBS/pluginsForBS.js"></script>
 <!-- Library 10+ miscellaneous plugins -->
-<script type="text/javascript" src="assets/plugins/miscellaneous/miscellaneous.js"></script>
+<script type="text/javascript" src="../template/assets/plugins/miscellaneous/miscellaneous.js"></script>
 <!-- Library Themes Customize-->
-<script type="text/javascript" src="assets/js/caplet.custom.js"></script>
+<script type="text/javascript" src="../template/assets/js/caplet.custom.js"></script>
 <script type="text/javascript">
 $(function() {
 		   //Login animation to center 
