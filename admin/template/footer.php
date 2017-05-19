f3aee70ab565062724b3d1654df01d8ad3b2ca02
asylzhan1997 <asik.clasher@gmail.com>
2017-05-19 13:32:37 +0500

    Убрал лишнее, разделил

---
+++ admin/template/footer.php
@@ -0,0 +1,41 @@
+		<!--
+		//////////////////////////////////////////////////////////////
+		//////////     LEFT NAV MENU     //////////
+		///////////////////////////////////////////////////////////
+		-->
+<? include("assets/inc/menu_inc/left_menu.php"); ?>
+		<!-- //nav left menu-->		
+</div>
+<!-- //wrapper-->
+
+
+<!--
+////////////////////////////////////////////////////////////////////////
+//////////     JAVASCRIPT  LIBRARY     //////////
+/////////////////////////////////////////////////////////////////////
+-->
+		
+<!-- Jquery Library -->
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.ui.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap/bootstrap.min.js"></script>
+<!-- Modernizr Library For HTML5 And CSS3 -->
+<script type="text/javascript" src="assets/js/modernizr/modernizr.js"></script>
+<script type="text/javascript" src="assets/plugins/mmenu/jquery.mmenu.js"></script>
+<script type="text/javascript" src="assets/js/styleswitch.js"></script>
+<script type="text/javascript" src="assets/js/styleswitch.js"></script>
+<!-- Library 10+ Form plugins-->
+<script type="text/javascript" src="assets/plugins/form/form.js"></script>
+<!-- Datetime plugins -->
+<script type="text/javascript" src="assets/plugins/datetime/datetime.js"></script>
+<!-- Library Chart-->
+<script type="text/javascript" src="assets/plugins/chart/chart.js"></script>
+<!-- Library  5+ plugins for bootstrap -->
+<script type="text/javascript" src="assets/plugins/pluginsForBS/pluginsForBS.js"></script>
+<!-- Library 10+ miscellaneous plugins -->
+<script type="text/javascript" src="assets/plugins/miscellaneous/miscellaneous.js"></script>
+<!-- Library Themes Customize-->
+<script type="text/javascript" src="assets/js/caplet.custom.js"></script>
+
+</body>
+</html>
---
+++ admin/template/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- Meta information -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+<!-- Title-->
+<title>CAPLET |  Admin HTML Themes</title>
+
+<!-- Favicons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+<!-- CSS Stylesheet-->
+<link type="text/css" rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css" />
+<link type="text/css" rel="stylesheet" href="assets/css/bootstrap/bootstrap-themes.css" />
+<link type="text/css" rel="stylesheet" href="assets/css/style.css" />
+
+<!-- Styleswitch if  you don't chang theme , you can delete -->
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style1" href="assets/css/styleTheme1.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style2" href="assets/css/styleTheme2.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style3" href="assets/css/styleTheme3.css" />
+<link type="text/css" rel="alternate stylesheet" media="screen" title="style4" href="assets/css/styleTheme4.css" />
+
+</head>
+<body>
+<div id="wrapper">
+		<!--
+		/////////////////////////////////////////////////////////////////////////
+		//////////     HEADER  CONTENT     ///////////////
+		//////////////////////////////////////////////////////////////////////
+		-->
+		<? include("assets/inc/menu_inc/top_menu.php"); ?>
+		<!-- //header-->
+		
+		
+		<!--
+		/////////////////////////////////////////////////////////////////////////
+		//////////     SLIDE LEFT CONTENT     //////////
+		//////////////////////////////////////////////////////////////////////
+		-->
+		
+		
+		<!--
+		/////////////////////////////////////////////////////////////////////////
+		//////////     TOP SEARCH CONTENT     ///////
+		//////////////////////////////////////////////////////////////////////
+		-->
+		<div class="widget-top-search">
+			<span class="icon"><a href="#" class="close-header-search"><i class="fa fa-times"></i></a></span>
+			<form id="top-search">
+					<h2><strong>Quick</strong> Search</h2>
+					<div class="input-group">
+							<input  type="text" name="q" placeholder="Find something..." class="form-control" />
+							<span class="input-group-btn">
+							<button class="btn" type="button" title="With Sound"><i class="fa fa-microphone"></i></button>
+							<button class="btn" type="button" title="Visual Keyboard"><i class="fa fa-keyboard-o"></i></button>
+							<button class="btn" type="button" title="Advance Search"><i class="fa fa-th"></i></button>
+							</span>
+					</div>
+			</form>
+		</div>
+		<!-- //widget-top-search-->
+		
+		
