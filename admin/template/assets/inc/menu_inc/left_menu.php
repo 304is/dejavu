f3f06c3c2f1679a5f8a69bb5a93990bf6d774332
redgold98 <anton.grobovenko@gmail.com>
2017-05-20 12:59:35 +0500

    Добавление ссылок в меню

--- admin/template/assets/inc/menu_inc/left_menu.php
+++ admin/template/assets/inc/menu_inc/left_menu.php
@@ -1,15 +1,20 @@
 <nav id="menu">
 		<ul>
-				<li><a href="#"><i class="icon  fa fa-home"></i> Главная </a></li>
-				<li><a href="#"><i class="icon  fa fa-th-large"></i> Товары </a></li>
+				<li><a href="http://praktika.local/admin/main/index.php"><i class="icon  fa fa-home"></i> Главная </a></li>
+				<li><span><i class="icon  fa fa-th-large"></i> Товары</span>
+						<ul>
+								<li><a href="#"><i class="icon  fa fa-plus"></i> Добавить товар </a></li>
+								<li><a href="http://praktika.local/admin/category/index.php"><i class="icon  fa fa-th"></i> Просмотреть товары </a></li>
+						</ul>
+					</li>
 				<li><span><i class="icon  fa fa-list"></i> Категории товаров</span>
-								<ul>
-										<li><a href="#"><i class="icon  fa fa-plus"></i> Добавить категорию </a></li>
-										<li><a href="#"><i class="icon  fa fa-th"></i> Просмотреть категории </a></li>
-								</ul>
-						</li>
-				<li><a href="#"><i class="icon  fa fa-user"></i> Пользователи </a></li>
-				<li><a href="#"><i class="icon  fa fa-comment"></i> Отзывы </a></li>
-				<li><a href="#"><i class="icon  fa fa-shopping-cart"></i> Заказы </a></li>
+					<ul>
+							<li><a href="#"><i class="icon  fa fa-plus"></i> Добавить категорию </a></li>
+							<li><a href="http://praktika.local/admin/category/index.php"><i class="icon  fa fa-th"></i> Просмотреть категории </a></li>
+					</ul>
+				</li>
+				<li><a href="http://praktika.local/admin/user/index.php"><i class="icon  fa fa-user"></i> Пользователи </a></li>
+				<li><a href="http://praktika.local/admin/review/index.php"><i class="icon  fa fa-comment"></i> Отзывы </a></li>
+				<li><a href="http://praktika.local/admin/order/index.php"><i class="icon  fa fa-shopping-cart"></i> Заказы </a></li>
 		</ul>
 </nav>
