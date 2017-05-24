4d023f517490593dbdbf5fd951ab6a7620e6fa05
mastamankaz <mastaman.kaz@mail.ru>
2017-05-24 17:32:01 +0500

    Add basket and view

--- template/checkout.php
+++ template/checkout.php
@@ -1,9 +1,14 @@
-<?$title='Deja Vu | Проверка '; ?>
+<?$title='Deja Vu | Проверка '; 
+include_once ("../lib/db.php");
+$rowis = fetchAll("SELECT goods.name,goods.price,basket.quantity,basket.date FROM `basket`
+LEFT JOIN goods ON goods.id=basket.id_goods");?>
 <?php require_once('header.php'); ?> 
 <!--cart-items-->
 	<div class="cart-items">
 		<div class="container">
-			<h2>Моя корзина (3)</h2>
+			<h2>Моя корзина (<span id="simpleCart_quantity" class="simpleCart_quantity"> 0 </span>) </h2>
+			<?php foreach ($rowis as $value) {
+                ?>  
 			<script>$(document).ready(function(c) {
 				$('.close1').on('click', function(c){
 					$('.cart-header').fadeOut('slow', function(c){
@@ -19,79 +24,22 @@
 						 <img src="images/m1.png" class="img-responsive" alt="">
 					</div>
 					<div class="cart-item-info">
-						<h3><a href="#"> Lorem Ipsum - это не просто </a><span>Время забирать:</span></h3>
+						<h3><a href="#"><?=$value["name"];?></a></h3>
 						<ul class="qty">
-							<li><p>Мин. стоимость заказа:</p></li>
 							<li><p>Бесплатная доставка</p></li>
 						</ul>
 						<div class="delivery">
-							<p>Плата За Обслуживание : $10.00</p>
-							<span>Доставлен в 1-1:30 часа</span>
+							<p>Стоимость : <?=$value["price"]*$value["quantity"];?>&#8376 (<?=$value["quantity"];?> шт)</p>
+							<span>Доставлен в <?=$value["date"];?></span>
 							<div class="clearfix"></div>
 						</div>	
 					</div>
 					<div class="clearfix"></div>
 				</div>
 			</div>
-			 <script>$(document).ready(function(c) {
-					$('.close2').on('click', function(c){
-							$('.cart-header2').fadeOut('slow', function(c){
-						$('.cart-header2').remove();
-					});
-					});	  
-					});
-			 </script>
-			<div class="cart-header2">
-				<div class="close2"> </div>
-				<div class="cart-sec simpleCart_shelfItem">
-					<div class="cart-item cyc">
-						 <img src="images/m2.png" class="img-responsive" alt="">
-					</div>
-					<div class="cart-item-info">
-						<h3><a href="#"> Lorem Ipsum - это не просто </a><span>Время забирать:</span></h3>
-						<ul class="qty">
-							<li><p>Мин. стоимость заказа:</p></li>
-							<li><p>Бесплатная доставка</p></li>
-						</ul>
-						<div class="delivery">
-							<p>Плата За Обслуживание : $10.00</p>
-							<span>Доставлен в 1-1:30 часа</span>
-							<div class="clearfix"></div>
-						</div>	
-					</div>
-				   <div class="clearfix"></div>
-				</div>
-			</div>
-				<script>$(document).ready(function(c) {
-					$('.close3').on('click', function(c){
-							$('.cart-header3').fadeOut('slow', function(c){
-						$('.cart-header3').remove();
-					});
-					});	  
-					});
-				</script>
-			<div class="cart-header3">
-				<div class="close3"> </div>
-				<div class="cart-sec simpleCart_shelfItem">
-					<div class="cart-item cyc">
-						 <img src="images/m3.png" class="img-responsive" alt="">
-					</div>
-					<div class="cart-item-info">
-						<h3><a href="#"> Lorem Ipsum - это не просто </a><span>Время забирать:</span></h3>
-						<ul class="qty">
-							<li><p>Мин. стоимость заказа:</p></li>
-							<li><p>Бесплатная доставка</p></li>
-						</ul>
-						<div class="delivery">
-							<p>Плата За Обслуживание : $10.00</p>
-							<span>Доставлен в 1-1:30 часа</span>
-							<div class="clearfix"></div>
-						</div>	
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>		
-		</div>
+			<?php
+                        };
+                ?>
 	</div>
 	<!--//checkout-->	
 <?php require_once('footer.php'); ?> 
