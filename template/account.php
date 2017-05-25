acd18c9c270b579f037022035110131c8888fd87
redgold98 <anton.grobovenko@gmail.com>
2017-05-25 18:03:13 +0500

    Разработка регистрации.

--- template/account.php
+++ template/account.php
@@ -7,7 +7,7 @@ include_once("../lib/db.php");
 	<div class="account">
 		<div class="container">
 			<div class="register">
-				<form> 
+				<form method = "post"> 
 					<div class="register-top-grid">
 						<h3>Личная информация</h3>
 						<div class="input">
@@ -50,28 +50,42 @@ include_once("../lib/db.php");
 							<span>ПОДТВЕРЖДЕНИЕ ПАРОЛЯ<label>*</label></span>
 							<input type="password" name="password_verify">
 						 </div>
+						 	<div class="register-but">
+					   <input name = "submit" type="submit" value="Отправить">
+					   <div class="clearfix"> </div>
+				</div>
 					</div>
 				</form>
 				<div class="clearfix"> </div>
-				<div class="register-but">
-				   <form>
-					   <input type="submit" value="Отправить">
-					   <div class="clearfix"> </div>
-				   </form>
-				</div>
+
 			</div>
 	    </div>
 	</div>
 	<!--//account-->
 <?php 
 	require_once('footer.php');
-	$surname = $_POST['surname'];
-	$name = $_POST['name'];
-	$patronymic = $_POST['patronymic'];
-	$login = $_POST['login'];
-	$email = $_POST['email'];
-	$address = $_POST['address'];
-	$telephone = $_POST['telephone'];
-	$password = $_POST['password']
-	$password_verify = $_POST['password_verify']
+	if (isset($_POST['submit'])) {
+		$surname = $_POST['surname'];
+		$name = $_POST['name'];
+		$patronymic = $_POST['patronymic'];
+		$login = $_POST['login'];
+		$email = $_POST['email'];
+		$address = $_POST['address'];
+		$telephone = $_POST['telephone'];
+		$password = $_POST['password'];
+		$password_verify = $_POST['password_verify'];
+		$role = 4;
+		$user_hash = 123;
+		$user_ip = 123;
+		$reg_date = 123;
+		$registration_query = "
+			INSERT INTO user(surname, name, patronymic, login, password, e-mail, address, telephone, reg_date, id_role, user_hash, user_ip)
+			VALUES ('$surname','$name','$patronymic',$login','$password','$email','$address','$telephone','$reg_date','$role','$user_hash','$user_ip')
+		";
+		if ($password == $password_verify){
+			InsertRow($registration_query);
+		} else {
+			echo "Пароли не совпадают.";
+		}
+	};
 ?>
