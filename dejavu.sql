72a25c4e5b6a752045fe5015bf92d35e4017353f
mastamankaz <mastaman.kaz@mail.ru>
2017-05-26 20:56:32 +0300

    Add New dejavu.sql

--- dejavu.sql
+++ dejavu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.4.15.7
+-- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 19 2017 г., 13:40
--- Версия сервера: 5.5.53
--- Версия PHP: 5.6.29
+-- Время создания: Май 26 2017 г., 20:55
+-- Версия сервера: 5.5.50
+-- Версия PHP: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,13 +26,20 @@ SET time_zone = "+00:00";
 -- Структура таблицы `basket`
 --
 
-CREATE TABLE `basket` (
+CREATE TABLE IF NOT EXISTS `basket` (
   `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL DEFAULT '1',
   `id_goods` int(11) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `date` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `basket`
+--
+
+INSERT INTO `basket` (`id`, `id_user`, `id_goods`, `quantity`, `date`) VALUES
+(39, 2, 1, 1, '2017-05-26 16:58:07');
 
 -- --------------------------------------------------------
 
@@ -40,10 +47,17 @@ CREATE TABLE `basket` (
 -- Структура таблицы `category`
 --
 
-CREATE TABLE `category` (
+CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'категории 1');
 
 -- --------------------------------------------------------
 
@@ -51,13 +65,23 @@ CREATE TABLE `category` (
 -- Структура таблицы `content`
 --
 
-CREATE TABLE `content` (
+CREATE TABLE IF NOT EXISTS `content` (
   `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `id_order` int(11) NOT NULL,
   `id_goods` int(11) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `price` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `content`
+--
+
+INSERT INTO `content` (`id`, `id_user`, `id_order`, `id_goods`, `price`, `quantity`) VALUES
+(18, 2, 0, 39, 1000, 1),
+(19, 2, 55, 39, 1000, 1),
+(20, 2, 56, 39, 1000, 1);
 
 -- --------------------------------------------------------
 
@@ -65,10 +89,17 @@ CREATE TABLE `content` (
 -- Структура таблицы `delivery`
 --
 
-CREATE TABLE `delivery` (
+CREATE TABLE IF NOT EXISTS `delivery` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `delivery`
+--
+
+INSERT INTO `delivery` (`id`, `name`) VALUES
+(1, 'Курьером');
 
 -- --------------------------------------------------------
 
@@ -76,7 +107,7 @@ CREATE TABLE `delivery` (
 -- Структура таблицы `goods`
 --
 
-CREATE TABLE `goods` (
+CREATE TABLE IF NOT EXISTS `goods` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `date` int(11) DEFAULT NULL,
@@ -84,22 +115,41 @@ CREATE TABLE `goods` (
   `price` int(11) NOT NULL,
   `id_category` int(11) DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `goods`
+--
+
+INSERT INTO `goods` (`id`, `name`, `date`, `description`, `price`, `id_category`, `active`) VALUES
+(1, 'Товар 1 ', 1494450000, 'описание', 1000, 1, 1),
+(2, 'Товар 2 ', 1494450000, 'описание', 1010, 1, 1),
+(3, 'Товар 3', 1494450000, 'описание', 1020, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `order`
+-- Структура таблицы `orders`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `id_delivery` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `id_status` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_status` int(11) NOT NULL DEFAULT '1',
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `name`, `price`, `id_delivery`, `date`, `id_status`, `id_user`) VALUES
+(55, '1444454', 1000, 1, '2017-05-26 17:34:54', 1, 2),
+(56, 'Заказы 1', 1000, 1, '2017-05-26 17:35:23', 1, 2),
+(57, '1495820612', 1000, 1, '2017-05-26 17:43:32', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -107,14 +157,14 @@ CREATE TABLE `order` (
 -- Структура таблицы `review`
 --
 
-CREATE TABLE `review` (
+CREATE TABLE IF NOT EXISTS `review` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_goods` int(11) NOT NULL,
   `comments` varchar(255) NOT NULL,
   `date` int(11) DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -122,11 +172,11 @@ CREATE TABLE `review` (
 -- Структура таблицы `role`
 --
 
-CREATE TABLE `role` (
+CREATE TABLE IF NOT EXISTS `role` (
   `id_role` int(11) NOT NULL,
   `name_role` varchar(32) NOT NULL,
   `key_role` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `role`
@@ -144,7 +194,7 @@ INSERT INTO `role` (`id_role`, `name_role`, `key_role`) VALUES
 -- Структура таблицы `status`
 --
 
-CREATE TABLE `status` (
+CREATE TABLE IF NOT EXISTS `status` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -155,7 +205,7 @@ CREATE TABLE `status` (
 -- Структура таблицы `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -171,7 +221,15 @@ CREATE TABLE `user` (
   `user_hash` varchar(32) NOT NULL,
   `user_ip` int(10) NOT NULL DEFAULT '0',
   `auth_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `surname`, `name`, `patronymic`, `login`, `password`, `e-mail`, `address`, `telephone`, `reg_date`, `active`, `id_role`, `user_hash`, `user_ip`, `auth_date`) VALUES
+(1, 'Mastaman', 'Mastaman', 'Kazakh', 'admin', '$2y$10$cb2CbMK3p27PLxM.Cf7HguzzxdjGL.isQ.Khfmcdlgj8i7PfMz9tW', 'example@mail.com', 'adress', '87554542552', 1151, 1, 1, 'hasyh', 0, '2017-05-09 02:17:17'),
+(2, 'Jonhs', 'Jonh', 'Jonhe', 'user', 'user', 'example', 'asdas', '4874884', 511515, 1, 1, '1155', 0, '2017-05-24 07:07:11');
 
 -- --------------------------------------------------------
 
@@ -179,14 +237,21 @@ CREATE TABLE `user` (
 -- Структура таблицы `valuation`
 --
 
-CREATE TABLE `valuation` (
+CREATE TABLE IF NOT EXISTS `valuation` (
   `id` int(11) NOT NULL,
   `id_goods` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `ball` int(11) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `valuation`
+--
+
+INSERT INTO `valuation` (`id`, `id_goods`, `id_user`, `ball`, `date`, `active`) VALUES
+(1, 2, 2, 5, '0000-00-00 00:00:00', 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -223,9 +288,9 @@ ALTER TABLE `goods`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `order`
+-- Индексы таблицы `orders`
 --
-ALTER TABLE `order`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -266,42 +331,42 @@ ALTER TABLE `valuation`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT для таблицы `content`
 --
 ALTER TABLE `content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT для таблицы `delivery`
 --
 ALTER TABLE `delivery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT для таблицы `goods`
 --
 ALTER TABLE `goods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT для таблицы `order`
+-- AUTO_INCREMENT для таблицы `orders`
 --
-ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=58;
 --
 -- AUTO_INCREMENT для таблицы `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `role`
 --
 ALTER TABLE `role`
-  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT для таблицы `status`
 --
@@ -311,12 +376,12 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT для таблицы `valuation`
 --
 ALTER TABLE `valuation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
