87f674e8525e946a6cb8871728c0e3367007bb45
redgold98 <anton.grobovenko@gmail.com>
2017-05-27 14:51:33 +0500

    Заполнение главной страницы в админ-меню

--- admin/main/index.php
+++ admin/main/index.php
@@ -3,6 +3,9 @@ include_once("../../lib/secure.php");
 include ("../template/header.php");?>
 <div class="greetings">
 	<h1>Привет Админ!</h1>
+	<div class="sticker">
+		<p><img src="../template/assets/img/hello.png"></p>
+	</div>
 </div>
 <!-- Здесь должен быть Ваш код -->
 <?php include ("../template/footer.php"); ?>
